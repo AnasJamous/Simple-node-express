4e5d4500187e948fc6fbbccb4c14f418235a035d
AnasJamous <anas.jam89@gmail.com>
2018-03-01 10:09:51 +0200

    step4_search

--- index.js
+++ index.js
@@ -9,21 +9,49 @@ const getTime = () => {
     return current_hour+':'+current_minute+':'+current_seconds
 }
 
-
+// The Home page :
 app.get('/', (req, res) => res.send('batata'))
 
-// GET method route
-app.get('/', function (req, res) {
-  res.send('GET request to the homepage')
-})
 
+// The test page : 
 app.get('/test', (req,res) =>
     res.send({status:200, message:'Ok'}
 ))
 
+// The time page :
 app.get('/time', (req,res) => {
-    
-    res.send({status:200, message:('Time: ',getTime() )})
+    res.send({status:200, message:('Time: ',getTime())})
 }) 
+
+// The Hello page :
+app.get('/hello/:id', (req,res) =>{
+    const id = req.params.id;
+    res.send({status : 200 , message: "hello"+" "+id})
+})
+
+
+// The search page
+app.get('/search?:s',(req,res)=>{
+    let url = req.url
+    let input = url.split('/search')
+    s = input[1]
+    console.log(input,s)
+
+    if(s != ''){
+        res.write(`
+        {status:200, message:"ok", ${s} }
+
+         ok  ` + s + `
+    `)
+        res.send()
+    }else{
+        res.write(`
+        {status:500,error:true,message:"you have to provide a search"}  
+        You have to provide a search
+    `)
+        res.send()
+    }
+})
+
 
 app.listen(3000, () => console.log('listening on port 3000!'))
